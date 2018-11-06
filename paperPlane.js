8f48e752007437111561db62b7177eed5f09d0f0
Manuel Alejandro <a01022089@itesm.mx>
2018-11-06 15:56:36 -0600

    Collition & objects

--- paperPlane.js
+++ paperPlane.js
@@ -2,7 +2,10 @@ var container;
 var camera, scene, raycaster, renderer;
 var mouse = new THREE.Vector2(), INTERSECTED, CLICKED;
 var radius = 100, theta = 0;
-var plane;
+var plane = null, planeBox = null,
+land = null,
+cone = null, coneBox = null,
+tree = null, treeBox = null;
 var currentTime = Date.now();
 var animation = "run";
 var tag = null;
@@ -11,6 +14,9 @@ var score = 0;
 var reset = null;
 var count = 0;
 var grass = "images/grass.jpg";
+
+
+var orbitControls = null;
 
 async function createLand(y)
 {
@@ -45,9 +51,60 @@ async function createPlane()
         {
             object.material = new THREE.MeshPhongMaterial({ map:0xf0f0f0});
             object.position.set(0,0,1);
-            object.scale.set(70, 70, 70);
+            object.scale.set(20, 20, 20);
             object.rotation.set(THREE.Math.degToRad(90), THREE.Math.degToRad(0), THREE.Math.degToRad(0));
             scene.add(object);
+            plane = object;
+        },
+    // called when loading is in progresses
+    function ( xhr ) { console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' ); },
+    // called when loading has errors
+    function ( error ) { console.log( 'An error happened' );}
+    );
+}
+
+async function createTree()
+{
+    var loader = new THREE.OBJLoader();
+    // load a resource
+    loader.load(
+    // resource URL
+        "objects/lowpolytree.obj",
+    // called when resource is loaded
+        function ( object ) 
+        {
+            object.material = new THREE.MeshPhongMaterial({ map:0xf0f0f0});
+            object.position.set(5,0,1.8);
+            object.scale.set(1, 1, 1);
+            object.rotation.set(THREE.Math.degToRad(90), THREE.Math.degToRad(0), THREE.Math.degToRad(0));
+            scene.add(object);
+            tree = object;
+        },
+    // called when loading is in progresses
+    function ( xhr ) { console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' ); },
+    // called when loading has errors
+    function ( error ) { console.log( 'An error happened' );}
+    );
+}
+
+async function createcone()
+{
+    var coneMap = new THREE.TextureLoader().load("objects/Texture/TCone.png");
+
+    var loader = new THREE.OBJLoader();
+    // load a resource
+    loader.load(
+    // resource URL
+        "objects/Traffic Cone.obj",
+    // called when resource is loaded
+        function ( object ) 
+        {
+            object.material = new THREE.MeshPhongMaterial({ map:coneMap});
+            object.position.set(0,0,0);
+            object.scale.set(0.5, 0.5, 0.5);
+            object.rotation.set(THREE.Math.degToRad(90), THREE.Math.degToRad(0), THREE.Math.degToRad(0));
+            scene.add(object);
+            cone = object;
         },
     // called when loading is in progresses
     function ( xhr ) { console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' ); },
@@ -77,6 +134,8 @@ async function createScene(canvas)
 
     await createPlane();
     await createLand(0);
+    await createTree();
+    await createcone()
 
     /*result = $("#result");
     score_l = $("#score");
@@ -91,8 +150,11 @@ async function createScene(canvas)
     });*/
         
     //document.addEventListener('keydown', onDocumentKeyDown);
+    orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
     window.addEventListener( 'resize', onWindowResize);
+
 }
+
 
 function onWindowResize() 
 {
@@ -108,6 +170,48 @@ function run()
 
     var now = Date.now();
     var deltat = now - currentTime;
-    currentTime = now;;
+    currentTime = now;
+
     KF.update();
+
+    if(orbitControls)
+        orbitControls.update();
+
+    if(plane)
+    {
+        planeBox = new THREE.Box3().setFromObject(plane);
+    }
+    if(tree)
+    {
+        tree.position.x -= 0.05; //movement
+        if(tree.position.x < -13)
+        {
+            tree.position.x = 13;
+            tree.position.y = Math.random() * 8 -4; //5 to -5
+        }
+        treeBox = new THREE.Box3().setFromObject(tree);
+    }
+    if(cone)
+    {
+        cone.position.x -= 0.02; //movement
+        if(cone.position.x < -13)
+        {
+            cone.position.x = 13;
+            cone.position.y = Math.random() * 8 -4; //5 to -5
+        }
+        coneBox = new THREE.Box3().setFromObject(cone);
+    }
+
+    detectCollision();
+
+}
+
+
+function detectCollision()
+{
+    if(planeBox && treeBox && planeBox.intersectsBox(treeBox))
+        console.log("Tree Collision");
+
+    if(planeBox && coneBox && planeBox.intersectsBox(coneBox))
+        console.log("cone Collision");
 }
